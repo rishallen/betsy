f39534b7e7d5dd7990b6ff20c0d40cacdfaccf14
Adriana <dri19tcc@gmail.com>
2016-05-02 16:25:37 -0700

    Added in migrations

--- db/migrate/20160502231634_create_products.rb
+++ db/migrate/20160502231634_create_products.rb
@@ -1,6 +1,13 @@
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
+      t.string :name
+      t.integer :price
+      t.integer :user_id
+      t.integer :stock
+      t.string :description
+      t.string :category
+      t.string :photo_url
 
       t.timestamps null: false
     end
