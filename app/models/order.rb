dcc5b858fc2fc4fb4fe3378a888fe51c840f7d73
Nicole Iwasaki <Nicole.A.Iwasaki@gmail.com>
2016-05-02 16:36:43 -0700

    validations for orders and order_items

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,8 +2,9 @@ class Order < ActiveRecord::Base
   validates :user_id, presence: true
   validates :status, presence: true
   validates :mailing_address, presence: true
-  validates :cc_digits, presence: true
+  validates :cc_digits, presence: true, length: { is: 4 }
   validates :expiration, presence: true
 
   has_many :order_items
+  belongs_to :users
 end
