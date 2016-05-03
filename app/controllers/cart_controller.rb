2e1d0b4d5efbef758c0c0e1e70c55c0887188dad
Adriana <dri19tcc@gmail.com>
2016-05-03 15:39:14 -0700

    Added methods I will be using to cart controller

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,2 +1,32 @@
 class CartController < ApplicationController
+  before_action :authenticate_user!
+  
+  def index
+    @cart = Cart.all
+  end
+
+  def new
+    @cart = Cart.new
+  end
+
+  def create
+    @cart = Cart.new
+    @cart.save
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def show
+
+  end
+
+  def current_user_cart
+    "cart#{current_user.id}"
+  end
 end
