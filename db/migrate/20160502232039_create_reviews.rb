3bef4ba510acffd59edb15636183098bb06a9664
Nicole Iwasaki <Nicole.A.Iwasaki@gmail.com>
2016-05-03 14:17:35 -0700

    fixed migrations, functional

--- db/migrate/20160502232039_create_reviews.rb
+++ db/migrate/20160502232039_create_reviews.rb
@@ -1,9 +1,9 @@
 class CreateReviews < ActiveRecord::Migration
   def change
     create_table :reviews do |t|
-      t.integer, :rating, null: false
-      t.integer, :user_id, null: false
-      t.integer, :product_id, null: false
+      t.integer :rating, null: false
+      t.integer :user_id, null: false
+      t.integer :product_id, null: false
       t.string :content
 
       t.timestamps null: false
