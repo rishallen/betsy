8c1abd94c315ee4a05429efdff1259eabafe8e0d
Suzanne Harrison <harrison_family@hotmail.com>
2016-05-03 15:13:07 -0700

    starting products controller on its own branch

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,27 @@
 class ProductsController < ApplicationController
+
+  def index
+      @all_products = Product.order(category: :asc)
+      render :index
+    end
+
+    def show
+      @product = Product.find(params[:id])
+      render :show
+    end
+
+    def new
+      @product = Product.new
+    end
+
+    def create
+      @product = Product.new(product_create_params[:product])
+      if(@product.save)
+        redirect_to product_path(@product.id)#redirect in case user tries to post another form - brings them to entered view
+      else
+        render :new
+      end
+    end
+
+
 end
