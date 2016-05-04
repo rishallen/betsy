2294431cf99e4fe5421abc34a44253ad71444a59
Nicole Iwasaki <Nicole.A.Iwasaki@gmail.com>
2016-05-04 15:09:05 -0700

    application controller method for current order

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :current_order
+
+  def current_order
+    if !session[:order_id].nil? && Order.find_by(order_id: session[:order_id]).status == "pending"
+      Order.find(session[:order_id])
+    else
+      Order.new(status: "pending")
+    end
+  end
 end
