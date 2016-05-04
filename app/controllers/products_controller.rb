55e73afeea0c6e025d3153e7dc84429ea3c0c2c9
Suzanne Harrison <harrison_family@hotmail.com>
2016-05-04 15:01:41 -0700

    fworked on products controller mothods, products, new, show, index views.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,27 +1,68 @@
 class ProductsController < ApplicationController
 
   def index
-      @all_products = Product.order(category: :asc)
-      render :index
-    end
+    @all_products = Product.order(category: :asc)
+    render :index
+  end
 
-    def show
-      @product = Product.find(params[:id])
-      render :show
-    end
+  def show
+    @product = Product.find(params[:id])
+    # @review = Review.new(product_id: @product.id)
+    # if @order_item = @current_order.orderitems.where("product_id = ?", params[:id]).first
+    # else
+    #   @order_item = Orderitem.new(product_id: @product.id)
+    # end
+    render :show
+  end
 
-    def new
-      @product = Product.new
-    end
+  def new
+    @product = Product.new
+    # @category = Product.find(1).category
+  end
 
-    def create
-      @product = Product.new(product_create_params[:product])
-      if(@product.save)
+  def by_category
+    @category = Product.find_by(category: params[:product])
+    @products = @category.products
+  end
+
+  def by_seller
+    @user = User.find(params[:id])
+    @products = @user.products.where(out_of_stock: false)
+  end
+
+  def create
+    @product = Product.new(product_params[:product])
+    if(@product.save)
         redirect_to product_path(@product.id)#redirect in case user tries to post another form - brings them to entered view
-      else
-        render :new
-      end
+    else
+      render :new
     end
+  end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @product.update(product_params)
+    if @product.save
+      redirect_to user_path(current_user)
+    else
+      render 'edit'
+    end
+  end
+
+  def out_of_stock
+    @product.out_of_stock ? @product.out_of_stock = false : @product.out_of_stock = true
+    @product.save
+    redirect_to user_path(@product.user_id)
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit([:name, :description, :price, :category, :picture_url, :stock, :user_id])
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+products = [
+  {
+    name: "Mustachifier",
+    price: 9.99,
+    user_id: 1,
+    stock: 20,
+    description: "Are you as tired of your baby’s lack of facial hair as I am? Well get ready my friends, because the days of mustache-less babies are NO MORE!",
+    category: "Baby",
+    photo_url: "http://shutupandtakemymoney.com/wp-content/uploads/2013/10/mustachifier-300x250.jpg"
+  },
+  {
+    name: "Glitter Beard Kit",
+    price: 99.99,
+    user_id: 2,
+    stock: 10,
+    description: "Celebrate every day with a festive Glitter Beard! So easy to do! AND you'll leave behind a little glitter everywhere you go!",
+    category: "Personal Care",
+    photo_url: "http://sobadsogood.com/uploads/media/2015/12/07/These-Glitter-Beards-Are-Sure-To-Get-You-In-The-Festive-Spirit.jpg"
+  },
+  {
+    name: "How To Guide to your Beard Bowl",
+    price: 29.99,
+    user_id: 3,
+
+    stock: 15,
+    description: "Why dirty the dishes?! Design your personal beard bowl today!",
+    category: "Personal Care",
+    photo_url: "http://i.kinja-img.com/gawker-media/image/upload/s--yLgLfF9M--/c_scale,fl_progressive,q_80,w_800/191643wh1xaatjpg.jpg"
+  }
+]
+
+products.each do |product|
+    Product.create(product)
+end
