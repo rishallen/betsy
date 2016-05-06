564d041fdb4c745feb310bdf72a4463cb2e2ffa3
Nicole Iwasaki <Nicole.A.Iwasaki@gmail.com>
2016-05-06 14:13:28 -0700

    Fixtures for orders, items, products + test model for order (not working yet)

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -4,4 +4,8 @@ class OrderTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "test db contains seeded order"
+    things = Order.find(1).order_items
+    assert_equal things.orders, 1
+    
 end
