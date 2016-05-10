bf23537349338a595c37853d0c08b47dc1384d91
Adriana <dri19tcc@gmail.com>
2016-05-10 15:49:19 -0700

    Added default value to products category

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20160510205410) do
-=======
-ActiveRecord::Schema.define(version: 20160510181617) do
->>>>>>> master
+ActiveRecord::Schema.define(version: 20160510224630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,17 +56,11 @@ ActiveRecord::Schema.define(version: 20160510181617) do
     t.integer  "user_id"
     t.integer  "stock"
     t.string   "description"
-    t.string   "category"
+    t.string   "category",    default: "Miscellaneous", null: false
     t.string   "photo_url"
-<<<<<<< HEAD
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.boolean  "status"
-=======
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
-    t.boolean  "status",      default: false, null: false
->>>>>>> master
+    t.datetime "created_at",                            null: false
+    t.datetime "updated_at",                            null: false
+    t.boolean  "status",      default: false,           null: false
   end
 
   create_table "reviews", force: :cascade do |t|
