ff961e28e489459a579ea3c13c00a9ed645f4a55
Suzanne Harrison <harrison_family@hotmail.com>
2016-05-11 14:58:25 -0700

    working on order testing.

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -4,10 +4,26 @@ class OrderTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
-  test "test db contains seeded order"
+  test "test db contains seeded order" do
     things = Order.find(1).order_items
     things.each do |item|
-      assert_equal item.order, 1
+      assert_equal item.order_id, 1
     end
+  end
+
+  test "does not save order without status" do
+    things = Order.new
+    assert_not things.save
+  end
+
+  test "saves order with status" do
+    things = Order.new(status: "pending")
+    assert things.save
+  end
+
+  test "order belongs to user" do
+    things = Order.find(1)
+    assert_equal things.user_id, 1
+  end
 
 end
