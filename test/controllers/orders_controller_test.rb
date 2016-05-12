a3d2d8755315813f334f702fd83201cd050dded4
Nicole Iwasaki <Nicole.A.Iwasaki@gmail.com>
2016-05-12 16:31:31 -0700

    Orders controller testing added but nonfunctional

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class OrdersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
+  # test "should get new" do
+  #   get :new, {status: "pending", user_id: users(:suze).id}
+  #   assert_response :success
+  #   assert_not_nil assigns(:order)
+  # end
+  #
+  # test "should create order" do
+  # assert_difference('Order.count') do
+  #   post :create, order: {status: "pending", user_id: users(:suze).id}
+  # end
+  #
+  # assert_redirected_to products_path
   # end
 end
