cdff67a51e1b68f757f4746026b1f24d1c5d5714
Nicole Iwasaki <Nicole.A.Iwasaki@gmail.com>
2016-05-12 14:01:49 -0700

    added tests for reviews

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class ReviewTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "test db contains seeded review" do
+    review = Review.find(1)
+    assert_equal review.id, 1
+  end
+
+  test "does not save review without number rating" do
+    review = Review.new(content: "reviewstuff")
+    assert_not review.save
+  end
+
+  test "does not save review without content" do
+    review = Review.new(rating: 1)
+    assert_not review.save
+  end
+
+  test "saves review with number rating and content" do
+    review = Review.new(rating: 3, content: "review")
+    assert review.save
+  end
+
+  test "review belongs to user" do
+    review = Review.find(1)
+    assert_equal review.user_id, 1
+  end
+
+
 end
