0a2112aa4c60ac6729e51452dbebf3dfef9cc456
Ania Gonzalez <aniagr@gmail.com>
2016-05-12 13:50:55 -0700

    Added model testing for products.

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "does not save product without name" do
+    prod = Product.new(price: 3, stock:100, user_id:1)
+    assert_not prod.save
+  end
+
+  test "does not save product without price" do
+    prod = Product.new(name: "Magic Beans", stock:100, user_id:1)
+    assert_not prod.save
+  end
+
+  test "does not save product without stock" do
+    prod = Product.new(name: "Magic Beans", price: 3, user_id:1)
+    assert_not prod.save
+  end
+
+  test "does not save product without user_id" do
+    prod = Product.new(name: "Magic Beans", price: 3, stock:100)
+    assert_not prod.save
+  end
+
+  test "saves product when given name, price, stock and user_id" do
+    prod = Product.new(name: "Magic Beans", price: 3, stock:100, user_id:1)
+    assert prod.save
+  end
+
+  test "product belongs to user" do
+    prod = Product.find(1)
+    assert_equal prod.user_id, 1
+  end
+
 end
